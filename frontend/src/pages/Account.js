8f0aae27cc7ca4e046a23903ada50e2048f6704a
Chevy20 <mattchevy2000@gmail.com>
2024-09-21 14:16:55 -0400

    Changing File Names and Structure

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RiotIdForm from '../components/RiotIdForm'; // Import the form component
 
-const RiotIdPage = () => {
+const AccountPage = () => {
     return (
         <div>
             <h1>Riot ID Search</h1>
@@ -11,4 +11,4 @@ const RiotIdPage = () => {
     );
 };
 
-export default RiotIdPage;
+export default AccountPage;
