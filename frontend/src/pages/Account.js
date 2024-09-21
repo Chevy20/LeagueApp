a0a2c85fbe66e111ea48d369036c6fcf23c27d01
Chevy20 <mattchevy2000@gmail.com>
2024-09-21 14:29:39 -0400

    Added account by PUUID

--- frontend/src/pages/Account.js
+++ frontend/src/pages/Account.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import RiotIdForm from '../components/RiotIdForm'; // Import the form component
+import PUUIDForm from '../components/PUUIDForm';
 
 const AccountPage = () => {
     return (
         <div>
-            <h1>Riot ID Search</h1>
-            {/* Render the RiotIdForm */}
-            <RiotIdForm />
+            <h1>Account API Test Page</h1>
+            <div>
+                <h2>Get Account by Riot ID</h2>
+                {/* Render the RiotIdForm */}
+                <RiotIdForm />
+            </div>
+            <div>
+                <h2>Get Account by PUUID</h2>
+                {/* Render the RiotIdForm */}
+                <PUUIDForm />
+            </div>
         </div>
     );
 };
